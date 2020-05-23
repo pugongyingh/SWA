c462e698c110fd6bcbb84dff7cba382b937f216c
pugongyingh <pgyhh@sina.cn>
2020-05-23 11:34:13 +0800

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,8 @@
 module.exports = {
-	mongoURI: process.env.MONGODB_URI,
-	mongoDbName: process.env.MONGO_DB_NAME,
-	mongoUserCollection: process.env.MONGO_USER_COLLECTION,
-	googleRickjwApiKey: process.env.GOOGLE_RICKJW_API_KEY,
+	mongoURI: "mongodb+srv://pgyh:mmmmmm88@cluster0-ddyp4.mongodb.net/test?retryWrites=true&w=majority",
+	mongoDbName: "pgyh",
+	mongoUserCollection: "pgyh",
+	: process.env.GOOGLE_RICKJW_API_KEY,
 	googleClientSecret: process.env.GOOGLE_OAUTH_CLIENT_SECRET,
 	googleClientID: process.env.GOOGLE_OAUTH_CLIENT_ID,
 	googleServerApiKey: process.env.GOOGLE_SERVER_API_KEY,
@@ -14,5 +14,5 @@ module.exports = {
 	githubClientSecret: process.env.GITHUB_CLIENT_SECRET,
 	githubCallbackUrl: process.env.GITHUB_APP_CALLBACK_URL,
 	jwtClientSecret: process.env.JWT_SECRET,
-	PORT: process.env.PORT
+	PORT: 8080
 }
